a0347a5c5d929b5fd64b4bdba892bcbeaecbb016
Rahardiann <yt005881@gmail.com>
2024-05-10 15:28:53 +0700

    tes

--- src/Components/loginform.js
+++ src/Components/loginform.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { FaUser, FaLock } from "react-icons/fa";
 import loginImage from "../assets/logo_kopi.png";
-import axios from "axios";
+
 
 export default function LoginForm() {
   const [user, setUser] = useState({
@@ -13,41 +13,17 @@ export default function LoginForm() {
     setUser((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
 
-  const handleLogin = async (e) => {
-    e.preventDefault();
-    let data = {
-      username: user.username,
-      password: user.password,
-    };
-
-    try {
-      const response = await axios.post(
-        "http://localhost:8080/user/login",
-        data
-      );
-
-      if (response.data.data.length === 0) {
-      } else {
-        sessionStorage.setItem("nama_user", response.data.data.nama_user);
-        sessionStorage.setItem("token", response.data.data.token);
-        sessionStorage.setItem("logged", response.data.data.logged);
-        sessionStorage.setItem("role", response.data.data.role);
-        sessionStorage.setItem("id_user", response.data.data.id_user);
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
+ 
 
   return (
     <div className="bg-main min-h-screen flex items-center justify-center">
       <div className="bg-main rounded-lg shadow-2xl p-8 w-full md:w-96">
         <img src={loginImage} alt="Login" className="mb-6 mx-auto max-w-20" />
-        <h2 className="text-center mb-8 text-xl font-semibold">
-          Sign In to Your Account
+        <h2 className="text-center mb-8 text-md text-gray-600">
+          Masuk Ke akun utama anda 
         </h2>
 
-        <form onSubmit={handleLogin}>
+        
           {/* Username Input */}
           <div className="relative mb-4">
             <div className="absolute top-1/2 left-4 transform -translate-y-1/2">
@@ -88,7 +64,7 @@ export default function LoginForm() {
           >
             Login
           </button>
-        </form>
+        
       </div>
     </div>
   );
